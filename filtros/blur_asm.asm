b304d7bf2d7f680a068a06ad6f492dca67209c03
hanimal <hanimal@bitbucket.org>
2015-09-24 21:43:54 -0300

    [master] Se agrega versiones de blur en C y ASM.

--- filtros/blur_asm.asm
+++ filtros/blur_asm.asm
@@ -2,22 +2,209 @@ default rel
 global _blur_asm
 global blur_asm
 
+extern getKernel
+
+%define TAM_PIXEL   4
+%define TAM_FLOAT   4
 
 section .data
 
-
-
+;cast_pixel1: db 0x80, 0x80, 0x80, 0x80, 0x01, 0x80, 0x80, 0x80, 0x02, 0x80, 0x80, 0x80, 0x03, 0x80, 0x80, 0x80
+cast_pixel1: db 0x00, 0x80, 0x80, 0x80, 0x01, 0x80, 0x80, 0x80, 0x02, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80
+;cast_pixel2: db 0x80, 0x04, 0x08, 0x0c, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80
+cast_pixel2: db 0x00, 0x04, 0x08, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80
 section .text
-;void blur_asm    (
-	;unsigned char *src,
-	;unsigned char *dst,
-	;int filas,
-	;int cols,
-    ;float sigma,
-    ;int radius)
+;void blur_asm (
+;               unsigned char *src,
+;               unsigned char *dst,
+;               int filas,
+;               int cols,
+;               float sigma,
+;               int radius)
 
 _blur_asm:
 blur_asm:
+    ;*------------------------------------------------------------------------*
+    ;* Parámetros de entrada
+    ;* --> RDI: puntero a imagen fuente
+    ;* --> RSI: puntero a imagen destino
+    ;* --> RDX: cantidad de filas de la imagen fuente
+    ;* --> RCX: cantidad de columnas de la imagen fuente
+    ;* --> R8: radio
+    ;* --> XMM0: sigma
+    ;*------------------------------------------------------------------------*
 
+    ; Armo la stack frame alineada
+    push rbp
+    mov  rbp, rsp
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+    sub  rsp, 8
+
+    ; Se guardan los parametros de entrada
+    mov r12, rdi
+    mov r13, rsi
+
+    xor r14, r14
+    xor r15, r15
+    xor rbx, rbx
+
+    mov r14d, edx
+    mov r15d, ecx
+    mov ebx , r8d
+
+    ; R12: puntero a imagen fuente
+    ; R13: puntero a imagen destino
+    ; R14: cantidad de filas de la imagen fuente
+    ; R15: cantidad de columnas de la imagen fuente
+    ; RBX: radio
+
+    ; Se obtiene la matriz de convolución
+    mov  rdi, rbx
+    call getKernel
+    mov  rdi, rax
+
+    ; RDI: matriz de convolución
+
+    ; Determino la dimensión de la matriz de convolusión: (2*radio)+1
+    mov rax, rbx
+    add rax, rbx
+    inc rax
+
+    ; RAX: dimensión de la matriz de convolusión
+
+    ; Recorro los píxeles de la imagen fuente de a uno sin considerar los que
+    ; estan en los bordes no aplicables (lo cual depende del radio) y para ello
+    ; defino:
+    ;
+    ; RCX: cantidad de filas a iterar
+    ; RDX: cantidad de columnas a iterar
+
+    ; Determino la primera fila de la imagen fuente donde aplico blur
+    movd xmm1, ebx          ; XMM1 = radio
+    movd xmm2, r15d         ; XMM2 = cantColumnas
+
+    pmuludq xmm1, xmm2      ; XMM1 = radio*cantColumnas
+
+    xor  rcx, rcx
+    movd ecx, xmm1          ; RCX = radio*cantColumnas
+
+    lea r13, [r13+rcx*TAM_PIXEL]
+
+    ; Recorro las filas de la imagen fuente
+    mov rcx, r14
+    sub rcx, rbx
+    sub rcx, rbx
+
+    movdqu xmm14, [cast_pixel1]
+    movdqu xmm15, [cast_pixel2]
+
+    .recorrerFilas:
+
+        ; Recorro los píxeles de la fila de la imagen fuente donde aplico blur
+        mov rdx, r15
+        sub rdx, rbx
+        sub rdx, rbx
+
+        lea r13, [r13+rbx*TAM_PIXEL]
+
+        .recorrerColumnas:
+
+            ; Guardo en la pila los punteros a la imagen fuente y la matriz de
+            ; convolución
+            push r12
+            push rdi
+
+            lea  r11, [rdi]
+            pxor xmm1, xmm1
+
+            ; R11: puntero auxiliar a la matriz de convolución
+            ; XMM1: Acumulador
+
+            ; Recorro la matriz de convolución y la imagen fuente en la región
+            ; determinada por el radio
+            mov r8, 0
+
+            .recorrerFilasKernel:
+
+                lea r10, [r12]
+
+                ; R10: puntero auxiliar a imagen fuente
+
+                mov r9, 0
+
+                .recorrerColumnasKernel:
+                    ; Voy acumulando el producto PIXEL[i][j]*K[i][j] en XMM1
+
+                    movd     xmm2, [r10]
+                    pshufb   xmm2, xmm14
+                    cvtdq2ps xmm2, xmm2     ; XMM2 = (float) PIXEL[i][j]
+
+                    movd     xmm3, [r11]    ; XMM3 = | 0 | 0 | 0 | K[i][j] |
+                    pshufd   xmm3, xmm3, 0  ; XMM3 = | K[i][j] | K[i][j] | K[i][j] | K[i][j] |
+
+                    mulps    xmm2, xmm3     ; XMM2 = PIXEL[i][j]*K[i][j]
+                    addps    xmm1, xmm2     ; XMM1 = XMM1 + PIXEL[i][j]*K[i][j]
+
+                    ; Avanzo al siguiente valor K[i][j] de la fila en la matriz
+                    ; de convolución y al siguiente valor PIXEL[i][j] de la fila
+                    ; en la imagen fuente de la región determinada por el radio
+                    lea r10, [r10+TAM_PIXEL]
+                    lea r11, [r11+TAM_FLOAT]
+
+                    inc r9
+                    cmp r9, rax
+
+                jne .recorrerColumnasKernel
+
+                ; Avanzo a la siguiente fila en la imagen fuente en la región
+                ; determinada por el radio
+                lea r12, [r12+r15*TAM_PIXEL]
+
+                inc r8
+                cmp r8, rax
+                
+            jne .recorrerFilasKernel
+
+            cvtps2dq  xmm1, xmm1
+            pshufb    xmm1, xmm15
+            ;movd      xmm2, [r13]
+            ;pslldq    xmm2, 15
+			;psrldq    xmm2, 15
+            ;por       xmm1, xmm2
+            movd     [r13], xmm1
+
+            pop rdi
+            pop r12
+
+            ; Avanzo al siguiente pixel de la fila al que aplico blur
+            lea r12, [r12+TAM_PIXEL]
+            lea r13, [r13+TAM_PIXEL]
+
+            dec rdx
+
+        ; Salto si hay columnas que recorrer
+        jnz .recorrerColumnas
+
+        ; Avanzo a la siguiente fila de la imagen donde voy a aplicar blur
+        lea r12, [r12+rbx*TAM_PIXEL]
+        lea r12, [r12+rbx*TAM_PIXEL]
+        lea r13, [r13+rbx*TAM_PIXEL]
+
+        dec rcx
+
+    ; Salto si hay filas que recorrer
+    jnz .recorrerFilas
+
+    add rsp, 8
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
 
     ret
--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -3,110 +3,89 @@
 #include <math.h>
 #include "../tp2.h"
 
+float *getKernel( int radius, float sigma){
+	int i, j;
+    float x, y, ePowBla, constante;
 
+	int n    = 2*radius+1;
+	float *k = (float *) malloc( n * n * sizeof(float ) ) ;
+	int tam  = (2*radius+1) * (2*radius+1) * sizeof(float ) ;
+    
+    i = 0;
+    while( i<= (2*radius ) ){
 
-void blur_c    (
-    unsigned char *src,
-    unsigned char *dst,
-    int cols,
-    int filas,
-    float sigma,
-    int radius)
+    	j = 0;
+    	while( j <= (2*radius )  ){
+    		x         = radius -i;
+    		y         = radius - j;
+    		ePowBla   = pow( 2.71828182846, -1.0 * ( ( x*x + y*y ) / (2.0*pow(sigma,2.0)) ) );
+			constante = (1.0/ (2.0*( 3.14159265359)* pow(sigma,2.0) ));
+
+    		k[(i*n) +j]  =  (constante *  ePowBla);
+
+    		j++;
+    	}
+    	i++;
+    }
+
+    return k;
+}
+
+void blur_c (
+              unsigned char *src,
+              unsigned char *dst,
+              int cols,
+              int filas,
+              float sigma,
+              int radius)
 {
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
-    float k [(2 * radius ) + 1][(2 * radius ) + 1] ;
-    int i = 0;
-    float total= 0;
-    while( i<= (2 * radius ) ){
-    	int j = 0;
-    	while( j <= (2 * radius )  ){
-    		float x = radius -i;
-    		float y = radius - j;
-    		float ePowBla = pow( 2.71828182846  , -1.0 * ( ( (x*x) + (y*y) ) / (2.0*(pow(sigma,2.0))) )  );
-			float constante = (1.0/ (2.0*( 3.14159265359)* pow(sigma,2.0) ));
-    		k[i][j]  =  constante *  ePowBla;
+    int i, j, aux_i, aux_j, x, y, n;
+    float promedio_r, promedio_g, promedio_b;
 
-    		// printf("%f | ", k[i][j] );
-    		
-    		total += k[i][j];
-    		// printf ("k[i][j] :%f \n" , k[i][j]);
-    		j++;
-    	}	
-    	// printf("\n" );
-    	i++;
-    }
-    // printf("total : %f \n" , total);
+    float * k = getKernel(radius,sigma);
 
+    n = (2*radius)+1;
 
     i = radius;
 	while(i< (filas - radius)){
-		// printf ("i :%i \n" , i);
-		int j = radius;
+
+		j = radius;
 		while(j < (cols -radius)  ){
-			// printf ("j :%i \n" , j);
-			float promedio_r = 0;
-			float promedio_g = 0;
-			float  promedio_b = 0;
-			int aux_i = i - radius;
-			int x =  0;
-			//while(aux_i <= (i + 2*radius) ){
+
+			promedio_r = 0;
+			promedio_g = 0;
+			promedio_b = 0;
+
+			aux_i = i - radius;
+			x     = 0;
+
 			while(aux_i <= (i + radius) ){
-				int aux_j = j - radius;
-				int y = 0;
-				//while(aux_j <= ( j + 2*radius ) ) {
+				aux_j = j - radius;
+				y     = 0;
 				while(aux_j <= ( j + radius ) ) {
-					bgra_t *src_pixel = (bgra_t*)&src_matrix[aux_i][aux_j *4];
+					bgra_t *src_pixel = (bgra_t*)&src_matrix[aux_i][aux_j*4];
 					
-					promedio_r += ((float)src_pixel->r * k[x][y]);
-					promedio_g +=  ((float)src_pixel->g * k[x][y]);
-					promedio_b +=  ((float)src_pixel->b * k[x][y]);
-					//aux_j = aux_j + 4;
+					promedio_r += ((float)src_pixel->r * k[(x*n) + y]);
+					promedio_g += ((float)src_pixel->g * k[(x*n) + y]);
+					promedio_b += ((float)src_pixel->b * k[(x*n) + y]);
 					aux_j ++;
 					y ++ ;
 				}
 				aux_i++;
 				x ++;
 			}
-			// promedio_r = promedio_r / ((2*radius)+1)*((2*radius)+1);
-			// promedio_g = promedio_g / ((2*radius)+1)*((2*radius)+1);
-			// promedio_b = promedio_b / ((2*radius)+1)*((2*radius)+1);
 
 			bgra_t *dst_pixel = (bgra_t*)&dst_matrix[i][j*4];
 			
 			dst_pixel->r = (unsigned char) promedio_r;
 			dst_pixel->g = (unsigned char) promedio_g;
 			dst_pixel->b = (unsigned char) promedio_b;
-			// dst_pixel->a = 255;
-			// printf ("(%i , %i , %i ) | " ,promedio_r , promedio_g,promedio_b);
-			// printf ("dst_pixel->r :%i ," ,promedio_r);
-			// printf ("dst_pixel->g :%i ," , promedio_b);
-			// printf ("dst_pixel->b :%i \n" , promedio_g);
-			//j = j + 4;
 			j ++;
 		}
-		// printf("\n" );
+
 		i++;
-
 	}
-
-}
-
-
-
-int relX( int x ){
-	return x;
-}
-
-int relY( int y ){
-	return y/4;
-}
-
-int absX( int x ){
-	return x;
-}
-
-int absY( int y ){
-	return y*4;
 }
